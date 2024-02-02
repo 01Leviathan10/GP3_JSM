99022ef37072400fa46d2c70390ad4c7eb31e70f
myusername <marko.milicevic@macrometa.com>
2024-02-02 19:58:23 +0100

    adding not complated feature component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './features.css';
 
-const Features = () => {
+const Features = ({title, text}) => {
   return (
-    <div>
-      Features
+    <div className='gpt3__features-container'>
+      <div className='gpt3__features-container__feature-title'>
+      </div>
+      <div>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container_feature-text'>
+        {text}
+      </div>
     </div>
   )
 }
